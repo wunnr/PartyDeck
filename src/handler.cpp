b67cfaf1bd4ad4e84fcfbe76b08ef978920a816b
wunnr <wunner@protonmail.com>
2025-03-20 16:21:02 -0600

    Style improvements, throw runtime_errors

--- src/handler.cpp
+++ src/handler.cpp
@@ -9,6 +9,8 @@
 #include "shared.h"
 #include "util.cpp"
 
+using Util::Quotes;
+
 class Handler{
 private:
     bool valid = false;
@@ -69,9 +71,13 @@ public:
         return valid;
     }
 
+    bool IsWin(){
+        return is_win;
+    }
+
     bool Test(){
         if (!steam_appdir.empty()){
-            path_gameroot = PATH_STEAM / "steamapps/common" / steam_appdir;
+            path_gameroot = PATH_STEAM/"steamapps/common"/steam_appdir;
         } else {
             // TODO: if game isn't a steam app, the client should ask the user somehow to locate the game's dir themselves
             // and save that information to a settings file somewhere
@@ -92,42 +98,42 @@ public:
         return true;
     }
 
-    bool CreateGameSymlinkDir(){
-        PATH_SYM = PATH_PARTY / "game" / uid;
-        if (!fs::exists(PATH_SYM)){
+    void CreateGameSymlinkDir(){
+        PATH_SYM = PATH_PARTY/"game"/uid;
+        if (fs::exists(PATH_SYM)){
             LOG("[Handler] Game sym directory already found, skipping");
-            return true;
+            return;
         }
         LOG("[Handler] Creating sym directory");
         if (Util::CopyDirRecursive(path_gameroot, PATH_SYM, true) != true){
-            return false;
+            throw std::runtime_error("CopyDirRecursive failed!");
         }
 
         // If json file has goldbergpath set, copy goldberg files to the specified dir in the symlink path
         if (path_goldberg.empty()){
             path src;
-            if (is_32bit) src = path(PATH_EXECDIR) / "data/goldberg/32/";
-            else src = fs::path(PATH_EXECDIR) / "data/goldberg/64/";
-            path dest = PATH_SYM / path_goldberg;
+            if (is_32bit) src = PATH_EXECDIR/"data/goldberg/32/";
+            else src = PATH_EXECDIR/"data/goldberg/64/";
+            path dest = PATH_SYM/path_goldberg;
 
-            if (Util::CopyDirRecursive(src, dest, false) == false){ return false; }
+            if (Util::CopyDirRecursive(src, dest, false) == false){
+                throw std::runtime_error("CopyDirRecursive failed!");
+            }
 
             // Set up local goldberg save dir to bind the profile saves onto using bwrap
-            path path_f_localsave = dest / "local_save.txt";
+            path path_f_localsave = dest/"local_save.txt";
             std::ofstream f_localsave(path_f_localsave.c_str());
             if (!f_localsave){
-                LOG("[Handler] Couldn't open goldberg local_save.txt to edit.");
-                return false;
+                throw std::runtime_error("Couldn't open goldberg local_save.txt to edit!");
             }
             f_localsave << "goldbergsave" << endl;
             f_localsave.close();
 
-            path path_f_steam_appid = dest / "steam_appid.txt";
+            path path_f_steam_appid = dest/"steam_appid.txt";
             if (fs::exists(path_f_steam_appid)) fs::remove(path_f_steam_appid);
             std::ofstream f_steam_appid(path_f_steam_appid.c_str());
             if (!f_steam_appid){
-                LOG("[Handler] Couldn't open goldberg steam_appid.txt to edit.");
-                return false;
+                throw std::runtime_error("Couldn't open goldberg steam_appid.txt to edit.");
             }
             f_steam_appid << steam_appid << endl;
             f_steam_appid.close();
@@ -136,21 +142,25 @@ public:
             string steam_dll;
             if (is_win) { steam_dll = (is_32bit) ? string("steam_api.dll") : string("steam_api64.dll"); }
             else { steam_dll = string("libsteam_api.so"); }
-            path path_find_interfaces = PATH_EXECDIR / "data/goldberg/find_interfaces.sh";
-            src = path_gameroot / path_goldberg / steam_dll;
-            dest = PATH_SYM / path_goldberg / "steam_interfaces.txt";
+            path path_find_interfaces = PATH_EXECDIR/"data/goldberg/find_interfaces.sh";
+            src = path_gameroot/path_goldberg/steam_dll;
+            dest = PATH_SYM/path_goldberg/"steam_interfaces.txt";
             int ret = Util::Exec(path_find_interfaces.string() + " \"" + src.string() + "\" > \"" + dest.string() + "\"");
-            if (ret != 0) { LOG("Couldn't create goldberg steam_interfaces.txt"); return false; }
+            if (ret != 0) {
+                throw std::runtime_error("Couldn't create goldberg steam_interfaces.txt");
+            }
         }
 
         for (const auto& path : copy_paths){
-            fs::path src = path_gameroot / path;
-            fs::path dest = PATH_SYM / path;
+            fs::path src = path_gameroot/path;
+            fs::path dest = PATH_SYM/path;
 
             if (!fs::exists(src)) continue;
             try {
                 if (fs::is_directory(src)){
-                    if (Util::CopyDirRecursive(src, dest, false) == false) return false;
+                    if (Util::CopyDirRecursive(src, dest, false) == false) {
+                        throw std::runtime_error("CopyDirRecursive failed!");
+                    }
                 } else {
                     // fs::copy_options::overwrite_existing doesn't seem to do its job, on my gcc at least
                     Util::RemoveIfExists(dest);
@@ -158,98 +168,96 @@ public:
                 }
             }
             catch (const fs::filesystem_error& e) {
-                LOG("Filesystem error: " << e.what() << ". Src: " << src << ", Dest: " << dest);
-                return false;
+                throw std::runtime_error("Filesystem error:" + string(e.what()) + ". Src: " + string(src) + ", Dest: " + string(dest));
             }
         }
 
         for (const auto& path : remove_paths){
-            fs::path dest = PATH_SYM / path;
+            fs::path dest = PATH_SYM/path;
             Util::RemoveIfExists(dest);
         }
 
-        string copypath = PATH_EXECDIR / "handlers" / uid / "copy";
+        string copypath = PATH_EXECDIR/"handlers"/uid/"copy";
         if(fs::exists(copypath) && fs::is_directory(copypath)){
-            if (Util::CopyDirRecursive(copypath, PATH_SYM, false) != true) return false;
+            if (Util::CopyDirRecursive(copypath, PATH_SYM, false) != true) {
+                throw std::runtime_error("CopyDirRecursive failed!"); \
+            }
         }
 
-        return true;
+        return;
     }
 
-    bool CreateCompatdataDir(){
-        path src = PATH_STEAM / "steamapps/compatdata" / steam_appid;
-        path dest = PATH_PARTY / "compatdata";
-        if (!fs::exists(dest)){
+    void CreateCompatdataDir(){
+        path src = PATH_STEAM/"steamapps/compatdata"/steam_appid;
+        path dest = PATH_PARTY/"compatdata";
+        if (fs::exists(dest)){
             LOG("[Handler] Compatdata already found, skipping");
-            return true;
+            return;
         }
 
         try {
             fs::copy(src, dest, fs::copy_options::recursive | fs::copy_options::copy_symlinks);
         }
         catch (const fs::filesystem_error& e) {
-            std::cerr << "Filesystem error: " << e.what() << std::endl;
-            return false;
+            throw std::runtime_error("Filesystem error:" + string(e.what()) + ". Src: " + string(src) + ", Dest: " + string(dest));
         }
 
         string wine_patch_hidraw = "WINEPREFIX=\"" + dest.string() + "/pfx" + "\" ";
         // TODO: Don't know if we can just call wine on a Steam Deck.
         // If not, replace this with a call to Proton/files/bin/wine
         wine_patch_hidraw += "wine reg add \"HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Services\\winebus\" /v \"DisableHidraw\" /t REG_DWORD /d 1";
-        LOG("Patching HIDraw from wine prefix for proper PlayStation controller support...");
+        LOG("Patching HIDRaw from wine prefix for proper PlayStation controller support...");
         if (Util::Exec(wine_patch_hidraw) != 0){
-            LOG("Failed to patch HIDraw from Wine prefix");
-            return false;
+            throw std::runtime_error("Failed to patch HIDRaw from Wine prefix");
         }
 
         // Wine seems to take a while to "close" a prefix after applying a regedit.
         // Probably related to the time needed in between opening Proton instances
         sleep(5);
-        return true;
+        return;
     }
 
-    bool CreateProfileUniqueDirs(std::vector<Player>& players){
+    void CreateProfileUniqueDirs(std::vector<Player>& players){
         for (const auto& p : players){
             string name = p.profile;
-            fs::path profilepath = PATH_PARTY / "profiles" / name;
+            fs::path profilepath = PATH_PARTY/"profiles"/name;
             for (const string& subdir : game_unique_paths){
-                if (fs::exists(profilepath / uid / subdir)) continue;
+                if (fs::exists(profilepath/uid/subdir)) continue;
                 try {
-                    fs::create_directories(profilepath / uid / subdir);
+                    fs::create_directories(profilepath/uid/subdir);
                 }
                 catch (const fs::filesystem_error& e) {
-                    std::cerr << "Filesystem error: " << e.what() << std::endl;
-                    return false;
+                    throw std::runtime_error("Filesystem error:" + string(e.what()));
                 }
             }
         }
-        return true;
+        return;
     }
 
-    bool WriteGameLaunchScript(std::vector<Player>& players){
+    void WriteGameLaunchScript(std::vector<Player>& players){
         // Create the bash script to run all the games
-        path path_f_run = PATH_PARTY / "run.sh";
+        path path_f_run = PATH_PARTY/"run.sh";
         std::ofstream f_run(path_f_run.c_str());
         if (!f_run){
-            LOG("[Handler] Couldn't open run.sh to edit. Exiting...");
-            return false;
+            throw std::runtime_error("Couldn't open the run script");
         }
 
         f_run << "#!/bin/bash" << endl;
         f_run << "export SDL_JOYSTICK_HIDAPI=0" << endl;
+        // TODO: Check for setting before doing this
         if (is_32bit){ // SDL2 COMPAT
             f_run << "export SDL_DYNAMIC_API=" << PATH_STEAM << "/ubuntu12_32/steam-runtime/usr/lib/i386-linux-gnu/libSDL2-2.0.so.0" << endl;
         } else { f_run << "export SDL_DYNAMIC_API=" << PATH_STEAM << "/ubuntu12_32/steam-runtime/usr/lib/x86_64-linux-gnu/libSDL2-2.0.so.0" << endl; }
         if (runtime == "proton"){
-            f_run << "export STEAM_COMPAT_CLIENT_INSTALL_PATH=\"" << PATH_STEAM << "\"" << endl;
-            f_run << "export STEAM_COMPAT_DATA_PATH=\"" << PATH_PARTY << "/compatdata\"" << endl;
-            f_run << "export SteamAppId=\"" << steam_appid << "\"" << endl;
-            f_run << "export SteamGameId=\"" << steam_appid << "\"" << endl;
+            f_run << "export STEAM_COMPAT_CLIENT_INSTALL_PATH=" << Quotes(PATH_STEAM) << endl;
+            f_run << "export STEAM_COMPAT_DATA_PATH=" << Quotes(PATH_PARTY/"compatdata") << endl;
+            f_run << "export SteamAppId=" << Quotes(steam_appid) << endl;
+            f_run << "export SteamGameId=" << Quotes(steam_appid) << endl;
         }
-        f_run << "cd \"" << PATH_SYM << "\"" << endl;
+        f_run << "cd " << Quotes(PATH_SYM) << endl;
 
         for (int i = 0; i < players.size(); i++){
-            path path_profile = PATH_PARTY / "profiles" / players[i].profile;
+            path path_profile = PATH_PARTY/"profiles"/players[i].profile;
 
             // Wine/Proton doesn't seem to like opening multiple instances rapidly in succession
             if (is_win && i > 0) f_run << "sleep 5" << endl;
@@ -262,33 +270,34 @@ public:
 
             f_run << "bwrap \\" << endl;
             f_run << "--dev-bind / / \\" << endl;
+            f_run << "--tmpfs /tmp \\" << endl;
 
             // Bind user's "steam" folder onto the symlink path's "goldbergsave" folder for individual saves
             if (!path_goldberg.empty()){
-                f_run << "--bind \"" << path_profile << "/steam\" ";
-                f_run << "\"" << PATH_SYM << "/" << path_goldberg << "/goldbergsave\" \\" << endl;
+                f_run << "--bind " << Quotes(path_profile/"steam") << " ";
+                f_run << Quotes(PATH_SYM/path_goldberg/"goldbergsave") << " \\" << endl;
             }
-            f_run << "--tmpfs /tmp \\" << endl;
 
             // TODO: These only work with proton runtime, not wine (non-steam games)! Find some way to make this plat
             if (is_win && win_unique_appdata){
-                f_run << "--bind \"" << path_profile << "/windata/AppData\" ";
-                f_run << "\"" << PATH_PARTY << "/compatdata/pfx/drive_c/users/steamuser/AppData\" \\" << endl;
+                f_run << "--bind " << Quotes(path_profile/"windata/AppData") << " ";
+                f_run << Quotes(PATH_PARTY/"compatdata/pfx/drive_c/users/steamuser/AppData") << " \\" << endl;
             }
+
             if (is_win && win_unique_documents){
-                f_run << "--bind \"" << path_profile << "/windata/Documents\" ";
-                f_run << "\"" << PATH_PARTY << "/compatdata/pfx/drive_c/users/steamuser/Documents\" \\" << endl;
+                f_run << "--bind " << Quotes(path_profile/"windata/Documents") << " ";
+                f_run << Quotes(PATH_PARTY/"compatdata/pfx/drive_c/users/steamuser/Documents") << " \\" << endl;
             }
 
             for (const string& subdir : game_unique_paths){
-                f_run << "--bind \"" << path_profile << "/" << uid << "/" << subdir << "\" ";
-                f_run << "\"" << PATH_SYM << "/" << subdir << "\" \\" << endl;
+                f_run << "--bind " << Quotes(path_profile/uid/subdir) << " ";
+                f_run << Quotes(PATH_SYM/subdir) << " \\" << endl;
             }
 
             // TODO: idek if this actually works LOLLLL
             if (!is_win && linux_unique_localshare){
-                f_run << "--bind \""<< PATH_PARTY << "/profiles/" << players[i].profile << "/share\" ";
-                f_run << "\"" << PATH_LOCAL_SHARE << "\" \\" << endl;
+                f_run << "--bind " << Quotes(path_profile/"share") << " ";
+                f_run << Quotes(PATH_LOCAL_SHARE) << " \\" << endl;
             }
 
             // TODO: Make this dynamic, to allow for e.g. 2 controllers per instance
@@ -298,15 +307,15 @@ public:
             }
 
             if (runtime == "scout"){
-                f_run << "\"" << PATH_STEAM << "/ubuntu12_32/steam-runtime/run.sh\" ";
+                f_run << Quotes(PATH_STEAM/"ubuntu12_32/steam-runtime/run.sh") << " ";
             } else if (runtime == "proton"){
                 // TODO: Make Proton version configurable
-                f_run << "\"" << PATH_STEAM << "/steamapps/common/Proton - Experimental/proton\" run ";
+                f_run << Quotes(PATH_STEAM/"steamapps/common/Proton - Experimental/proton") << " run ";
             } else if (runtime == "wine"){
                 f_run << "wine ";
             }
 
-            f_run << "\"" << PATH_SYM << "/" << exec << "\"";
+            f_run << Quotes(PATH_SYM/exec);
             for (const string& arg : args){
                 if (arg == "$PROFILENAME") { f_run << " " << players[i].profile; }
                 else { f_run << " " << arg; }
@@ -320,20 +329,19 @@ public:
         f_run.close();
 
         if (chmod(path_f_run.c_str(), S_IRWXU) != 0) {  // S_IRWXU gives the user read, write, execute permissions
-            LOG("[party] Failed changing file permissions");
-            return false;
+            throw std::runtime_error("Failed changing run.sh permissions");
         }
 
-        return true;
+        return;
     }
 
 };
 
 std::vector<Handler> scanHandlers(){
     std::vector<Handler> out;
-    path dir_games = PATH_EXECDIR / "handlers/";
+    path dir_games = PATH_EXECDIR/"handlers/";
     for (const auto& entry : fs::directory_iterator(dir_games)){
-        if (fs::is_directory(entry) && fs::exists(entry.path() / "info.json")) {
+        if (fs::is_directory(entry) && fs::exists(entry.path()/"info.json")) {
             string entrystring = entry.path().string();
             Handler h(entrystring);
             if (h.Valid()) { out.push_back(h); }
